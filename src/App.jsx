bda496bb9234ca0ba9da4f3b45268409d0cc96ba
GabrielFr0st <gabriel.rozas.c@gmail.com>
2024-08-06 19:03:04 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './components/navbar';
+import Home from './components/home';
+import Footer from './components/footer';
+
+const App = () => (
+  <div>
+    <Navbar />
+    <Home />
+    <Footer />
+  </div>
+);
+
+export default App;
