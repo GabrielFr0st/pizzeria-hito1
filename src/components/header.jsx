be6334336820bc8ff02e11feac93d30278a9bb62
GabrielFr0st <165584874+GabrielFr0st@users.noreply.github.com>
2024-08-06 19:17:11 -0400

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Header = () => (
-  <header className="bg-dark text-white text-center py-5" style={{ backgroundImage: 'url("https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fpizza-banner.jpg?alt=media&token=89b2d6d3-5f6a-4cd7-9408-cc9c5e43c6b5")', backgroundSize: 'cover', backgroundPosition: 'center' }}>
+  <header className="bg-dark text-white text-center py-5" style={{ backgroundImage: 'url("https://img.freepik.com/free-photo/top-view-pepperoni-pizza-with-mushroom-sausages-bell-pepper-olive-corn-black-wooden_141793-2158.jpg?t=st=1722986190~exp=1722989790~hmac=443169610f99f7d66a33e26667e849c4d45a1f7783a0252ed568859dee8fefcf&w=1380")', backgroundSize: 'cover', backgroundPosition: 'center' }}>
     <h1>¡Pizzería Mamma Mia!</h1>
     <p>¡Tenemos las mejores pizzas que podrás encontrar!</p>
   </header>
